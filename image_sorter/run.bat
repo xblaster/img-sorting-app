9aca0f53c362fbc10f27eca2926da3c751a2f214
jerome wax <xblaster@gmail.com>
2024-07-21 15:38:06 +0200

    add run for automation

---
+++ image_sorter/run.bat
@@ -0,0 +1,3 @@
+python sorter.py "P:\OneDrive G\Images\Pellicule" "P:\Pictures" --qualifier "Gregoire" --filter-pxl
+python sorter.py "P:\OneDrive P\Images\Pellicule" "P:\Pictures" --qualifier "Pauline" --filter-pxl
+python sorter.py "C:\Users\nospa\OneDrive\Images\Pellicule" "P:\Pictures" --qualifier "Jerome" --filter-pxl
